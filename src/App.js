9cfc3fabf737732e93ab6f61cffa2904c291aaaf
Robby Lewis <Robbysim37@gmail.com>
2023-10-04 15:11:43 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a mailto="Robbysim37@gmail.com">Test</a>
+    </div>
+  );
+}
+
+export default App;

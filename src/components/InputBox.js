8dee7af77a524fa828b8d49ecd1d445dcce098dc
Robby Lewis <Robbysim37@gmail.com>
2023-10-05 15:26:11 -0400

    added inputs and email body

---
+++ src/components/InputBox.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./InputBox.css"
+
+const InputBox = (props) => {
+
+    const inputChangeHandler = (e) => {
+
+        console.log(props.emailDetails)
+        props.setEmailDetails(
+            {...props.emailDetails,
+            [`${props.id}`]:e.target.value}
+        )
+    }
+
+  return (
+    <div className="container">
+        <p>{props.label}</p>
+
+        <input value={props.emailDetails[`${props.id}`]} 
+        onChange={inputChangeHandler}
+        ></input>
+    </div>
+  )
+}
+
+export default InputBox

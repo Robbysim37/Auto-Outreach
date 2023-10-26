71b17f8e6d412fc4d9871de8590950977f91cc77
Robby Lewis <Robbysim37@gmail.com>
2023-10-26 19:03:28 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
   )
 
   const email =
-`Dear ${emailDetails.name}\n,
+`Dear ${emailDetails.name},\n
 My name is Robert Lewis, and I'm eager to introduce myself as a software developer interested in the ${emailDetails.position} position that has recently opened at ${emailDetails.company}. During my education, I've developed a passion for programming and a foundation in key technologies like HTML, CSS, React, JavaScript, TypeScript, and Node.js.\n
-I've focused on learning the fundamentals of React and Typescript to create user-friendly interfaces. I've also gained insights into building backend systems using Node.js. My enthusiasm and willingness to learn have allowed me to develop projects for local business as a Freelance Developer and I'm looking to bring those skills to a more professional environment.\n
+During my time at Bloomtech Institute of Technology, I've focused on learning the fundamentals of React and Typescript to create user-friendly interfaces. I've also gained insights into building backend systems using Node.js. My enthusiasm and willingness to learn have allowed me to develop projects for local business as a Freelance Developer and I'm looking to bring those skills to a more professional environment.\n
 While I have already applied through the regular channels,  I've attached my resume for your consideration. Please feel free to contact me at Robbysim37@gmail.com or 734-308-9797 to discuss how my initiative and dedication can help your team\n
 Thank you for your time and consideration.
 -Robert Lewis\n
